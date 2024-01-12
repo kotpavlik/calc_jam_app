e1285b8406644fb374808cb027c5b2dca4a3fc61
kotpavlik <kotpavlik88@gmail.com>
2024-01-12 18:33:53 +0300

    mazafucker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
       <div className="  w-full  items-center justify-center font-mono text-sm lg:flex">
-        <div className='w-auto h-screen flex items-center justify-center'>Hello guys I'am jam bot and my name is Denchick 🤟🏻</div>
+        <div className='w-auto h-screen flex items-center justify-center'>Hello guys I'am jam bot and my name is Denchick Mazafucker 🤟🏻</div>
       </div>
     </main>
   )
